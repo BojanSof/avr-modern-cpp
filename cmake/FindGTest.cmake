3783886278f8291d904c8fd940b37c45c41da544
Bojan Sofronievski <bojan.drago@gmail.com>
2023-01-22 20:13:20 +0100

    Added test preset, changed `add_test` with `gtest_discover_tests` for better tests discovery

--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -4,6 +4,7 @@ CPMAddPackage(
     NAME googletest
     GITHUB_REPOSITORY google/googletest
     GIT_TAG main
+    EXCLUDE_FROM_ALL true
     OPTIONS
         "INSTALL_GTEST OFF"
         "gtest_force_shared_crt"
